89233a7eba1e97c9774e3f2d59cbc223f14fee9d
MatthiasKiss <matthias.kiss@tuwien.ac.at>
2020-12-01 11:40:37 +0100

    Small changes to linear regression, path for data set is correct now.

--- RegressionAlgorithms/linearRegression.py
+++ RegressionAlgorithms/linearRegression.py
@@ -9,7 +9,7 @@ BEWARE: using an exact solution leads to division by zero because the RMSE is ze
         but should not be happening with real data anyways...
 
 
-linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterion = 1e-4, adaptAlpha = False, printOutput = False):
+linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterion = 1e-4, adaptAlpha = False, printOutput = False, maxIterations = None):
 
 Input Parameters:
 1) X and y are analogous in use to scikit learn use. But MUST be pandas arrays here
@@ -22,6 +22,7 @@ Input Parameters:
 7) initialWeigts, if set to random weights are initialized randomly, if set to manual, then an array with the weights according to:
         Y = w1*X1 + w2*X2 + ... + wB*Xb <- here Xb is the bias column (=[1]*NVal)
    needs to be specified.
+8) maxIterations sets the maximum number of iterations for the regression loop
 '''
 #using functions for loss function and derivative for now because it is more easily readable that way I think
 #probably less performant in python though...?
@@ -38,7 +39,11 @@ def updateWeights(X, y, yPred, weightsOld, lossFunction, alpha):
     if lossFunction == 'RMSE':
         loss = RMSE(y, yPred)
         for i in range(0, NWeights):
+            #print('weightsOld[i] = ', weightsOld[i])
+            #print('weightsNew[i] = ', weightsNew[i])
+            #print('ddwRMSE(i, X, y, yPred, weightsOld, loss = ',ddwRMSE(i, X, y, yPred, weightsOld, loss))
             weightsNew[i] = weightsOld[i] - alpha * ddwRMSE(i, X, y, yPred, weightsOld, loss)
+            #print('weightsNew[i] = ', weightsNew[i])
     else:
         print('ERROR: lossFunction = ', lossFunction, ' not found!')
         loss = 0
@@ -46,7 +51,7 @@ def updateWeights(X, y, yPred, weightsOld, lossFunction, alpha):
     
     return(weightsNew, loss)
 
-def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterion = 1e-4, adaptAlpha = False, printOutput = False, initialWeights = 'Random'):
+def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterion = 1e-4, adaptAlpha = False, printOutput = False, initialWeights = 'Random', maxIterations = None):
     NVar = len(X.columns)
     XCount = X.count()   
 
@@ -73,16 +78,20 @@ def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterio
         weights = np.random.rand(NVar)
     if initialWeights == 'Manual':
         weights = initialWeights
-    #weights = np.array([3.00001,1.00001,2.00001])
+    weights = np.array([0.223, 0.892, 3.347, 4.661])
     if printOutput == True:
         print('Initial weights = ', weights)    
         
     loss = convergenceCriterion + 1
     counter = 0
     while loss > convergenceCriterion:
+        if maxIterations == counter:
+            break
         if adaptAlpha == True:
-            if alpha > loss/10:
-                alpha /= 10
+            #if alpha > loss/10:
+            #    alpha /= 10
+            if alpha > loss*1e-7:
+                alpha /= 100
         
         #eqaution to be solved:
         #Y = w0*X0 + w1*X1 + w2*X2 ...<- here X0 is the bias column (=[1]*NVal)
@@ -95,32 +104,46 @@ def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterio
             print('iteration = ', counter)
             print('loss =', loss)
             print('alpha =', alpha)
-            print('updated weights = ', weights)    
+            print('updated weights = ', weights)
+            #print('y =', y)
+            #print('yPred =', yPred)
         counter += 1
     return()
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('../MetroInterstateTrafficVolumeTest.csv', delimiter = ',')
+    path = '../MetroInterstateTrafficVolume/MetroInterstateTrafficVolume.csv'
+    data = pd.read_csv(path, delimiter = ',')
     
-    #make it so that number of columns doesn't matter and deal with one hot encoding later
-    #X = data.drop(['traffic_volume', 'holiday', 'weather_main', 'weather_description', 'date_time'], axis=1)
+    #X = data.drop(['rain_1h', 'snow_1h', 'traffic_volume', 'holiday', 'weather_main', 'weather_description', 'date_time'], axis=1)
     #y = data['traffic_volume']
-    X = pd.DataFrame(data = np.array([[1, 1], [1, 2], [2, 2], [2, 3]]))
-    y = pd.DataFrame(data = (np.dot(X, np.array([1, 2])) + 3))
+    #X = pd.DataFrame(data = np.array([[1, 1], [1, 2], [2, 2], [2, 3]]))
+    #y = pd.DataFrame(data = (np.dot(X, np.array([1, 2])) + 3))
+    x = np.linspace(0,200, num = 200)
+    x1 = x+2
+    x2 = x*4-3
+    x3 = x*15-7
+    xges = np.zeros((200,3))
+    for i in range(0, 200):
+        xges[i, 0] = x1[i]
+        xges[i, 1] = x2[i]
+        xges[i, 2] = x3[i]
+    X = pd.DataFrame(data = xges)
+    y = pd.DataFrame(data = (np.dot(X, np.array([1, 2, 3])) + 4))
+    
+    
     print(X)
     print(y)
-    
-    linearRegression(X, y, alpha = 1, convergenceCriterion = 1e-5, adaptAlpha = True, printOutput = True)
-    
-    print('==================================================')
-    X = pd.DataFrame(data = np.array([[1, 1], [1, 2], [2, 2], [2, 3]]))
-    y = pd.DataFrame(data = (np.dot(X, np.array([1, 2])) + 3))
-    
+
     reg = LinearRegression(normalize = False).fit(X, y)
     print('reg.score(X,y) =', reg.score(X, y))
     print('reg.coef_ = ', reg.coef_)
     print('reg.intercept_ = ',reg.intercept_)
+    
+    print('==================================================')
+    
+    linearRegression(X, y, alpha = 1e-9, convergenceCriterion = 1e-5, adaptAlpha = True, printOutput = True)
+    
     
 
 
