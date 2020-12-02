bd33082a205a3c98dfb1227d1f6a4a539e61399b
MatthiasKiss <matthias.kiss@tuwien.ac.at>
2020-12-02 10:16:42 +0100

    Added the option to have alpha change over time and to set an alpha for each weight. Also added Mean Square Error (MSE) as well as Mean Absolute Error (MAE) for loss functions.
    Included a version off the linear regression,
    that solves the minimization problem of the square
    error in form of a matrix equation (uses scipy.linalg.solve(). To show,
    that that is indeed how the scikit learn linear regression solves the
    problem. This does not need a learning rate or initial weigths and
    therefore is a lot more stable and faster...
    
    Also the code is extremely slow compared to the matrix version. Could
    probably be optimized a lot because I used a lot of "unneccessary" function
    calls for the sake of readability.
    
    The linearRegression() calssifier is EXTREMELY sensitive to initial
    weights and choice of alpha and only converges when alpha is chosen
    well.

--- RegressionAlgorithms/linearRegression.py
+++ RegressionAlgorithms/linearRegression.py
@@ -3,6 +3,8 @@ import numpy as np
 import pylab as plt
 from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
+import time
+import scipy as scipy
 '''
 Calculate linear regression, using specified loss function.
 BEWARE: using an exact solution leads to division by zero because the RMSE is zero. Should be solved somehow... 
@@ -26,34 +28,72 @@ Input Parameters:
 '''
 #using functions for loss function and derivative for now because it is more easily readable that way I think
 #probably less performant in python though...?
+def Rsquare(y, yPred):
+    return(1 - float(y.subtract(yPred, axis = 0).pow(2).sum() / y.subtract(y.mean(), axis = 0).pow(2).mean()))
+
+#this is dumb... the whole using of functions instead of ifs 
+#... probably worse than an if statement in the core iteration?
+#this way i feel like it is super readble though and I can still change it if performance is a big issue
+#for performance reasons it might also be better to transform everything to numpy arrays?
+def alphaConst(alpha0, counter, mu):
+    return(alpha0)
+
+def alphaLin(alpha0, counter, mu):
+    return(alpha0/(counter*mu))
+
+def alphaSqrt(alpha0, counter, mu):
+    return(alpha0/np.sqrt(counter*mu))
+
 def RMSE(y, yPred):
     return(float(np.sqrt(y.subtract(yPred, axis = 0).pow(2).mean())))
-    #return(np.sqrt(np.mean(np.square(y - yPred))))
 
-def ddwRMSE(i, X,  y, yPred, weights, RMSE):
-    return(float(np.mean(y.subtract(yPred, axis = 0).multiply(-X.iloc[:,i], axis=0))) / (RMSE))
+def ddwRMSE(i, X,  y, yPred, weights):
+    return(float(np.mean(y.subtract(yPred, axis = 0).multiply(-X.iloc[:,i], axis=0))) / (RMSE(y,yPred)))
 
-def updateWeights(X, y, yPred, weightsOld, lossFunction, alpha):
+def MSE(y, yPred):
+    return(float(y.subtract(yPred, axis = 0).pow(2).mean()))
+
+def ddwMSE(i, X,  y, yPred, weights):
+    return(2*float(np.mean(y.subtract(yPred, axis = 0).multiply(-X.iloc[:,i], axis=0))))
+
+def MAE(y, yPred):
+    return(float(y.subtract(yPred, axis = 0).abs().mean()))
+
+def ddwMAE(i, X,  y, yPred, weights):
+    return(float(np.mean(np.sign(y.subtract(yPred, axis = 0)).multiply(-X.iloc[:,i], axis=0))))
+
+def updateWeights(X, y, yPred, weightsOld, alpha, lossFkt, ddwLossFkt):
     NWeights = len(weightsOld)
     weightsNew = np.zeros(NWeights)
-    if lossFunction == 'RMSE':
-        loss = RMSE(y, yPred)
-        for i in range(0, NWeights):
-            #print('weightsOld[i] = ', weightsOld[i])
-            #print('weightsNew[i] = ', weightsNew[i])
-            #print('ddwRMSE(i, X, y, yPred, weightsOld, loss = ',ddwRMSE(i, X, y, yPred, weightsOld, loss))
-            weightsNew[i] = weightsOld[i] - alpha * ddwRMSE(i, X, y, yPred, weightsOld, loss)
-            #print('weightsNew[i] = ', weightsNew[i])
-    else:
-        print('ERROR: lossFunction = ', lossFunction, ' not found!')
-        loss = 0
-        weightsNew = weightsOld    
-    
+    loss = lossFkt(y, yPred)
+    for i in range(0, NWeights):
+        weightsNew[i] = weightsOld[i] - alpha[i] * ddwLossFkt(i, X, y, yPred, weightsOld)
     return(weightsNew, loss)
 
-def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterion = 1e-4, adaptAlpha = False, printOutput = False, initialWeights = 'Random', maxIterations = None):
+def linearRegression(X, y, lossFunction = 'RMSE', alpha = [], mu = 1, convergenceCriterion = 1e-4, alphaMethod = 'lin', 
+                     printOutput = False, initialWeights = 'Zero', maxIterations = None):
+    
+    lossDict = {
+        "RMSE": RMSE,
+        "MSE": MSE,
+        "MAE": MAE
+    }
+    
+    ddwLossDict = {
+        "RMSE": ddwRMSE,
+        "MSE": ddwMSE,
+        "MAE": ddwMAE
+    }
+    
+    alphaMethodDict = {
+        "const" : alphaConst,
+        "lin" : alphaLin,
+        "sqrt" : alphaSqrt
+    }
+
     NVar = len(X.columns)
     XCount = X.count()   
+
 
     #assuming all columns have been preprocessed to have the same number of values!
     #check if all columns are of same length, error otherwise:
@@ -63,7 +103,7 @@ def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterio
     if all(x == lenCols[0] for x in lenCols) == True:
         NVal = X.count()[0]
     else:
-        print('ERROR: Columns have different number of values!')
+        print('ERROR: Columns have different numbers of values!')
         return()
     
     #because it is easier to do the derivative later by not needing an exception for the bias weight 
@@ -73,32 +113,34 @@ def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterio
     X.insert(loc = NVar, column = 'bias', value = biasCol)
     NVar += 1
 
+    if alpha == []:
+        alpha = np.ones(NVar)
+    alpha0 = alpha
     #weigths for each column plus bias
+    if initialWeights == 'Zero':
+        weights = np.zeros(NVar)
     if initialWeights == 'Random':
         weights = np.random.rand(NVar)
     if initialWeights == 'Manual':
         weights = initialWeights
-    weights = np.array([0.223, 0.892, 3.347, 4.661])
+    #weights = np.array([0.223, 0.892, 3.347, 4.661])
     if printOutput == True:
         print('Initial weights = ', weights)    
         
     loss = convergenceCriterion + 1
-    counter = 0
+    counter = 1
+
     while loss > convergenceCriterion:
         if maxIterations == counter:
             break
-        if adaptAlpha == True:
-            #if alpha > loss/10:
-            #    alpha /= 10
-            if alpha > loss*1e-7:
-                alpha /= 100
+        alpha = alphaMethodDict[alphaMethod](alpha0, counter, mu)
         
         #eqaution to be solved:
         #Y = w0*X0 + w1*X1 + w2*X2 ...<- here X0 is the bias column (=[1]*NVal)
         yPred = X.multiply(weights, axis = 1).sum(axis = 1)
         
         #update weights and calc loss function and its derivative:
-        weights, loss = updateWeights(X, y, yPred, weights, lossFunction, alpha)
+        weights, loss = updateWeights(X, y, yPred, weights, alpha, lossDict[lossFunction], ddwLossDict[lossFunction])
         if printOutput == True:
             print('===================================')
             print('iteration = ', counter)
@@ -108,8 +150,42 @@ def linearRegression(X, y, lossFunction = 'RMSE', alpha = 1, convergenceCriterio
             #print('y =', y)
             #print('yPred =', yPred)
         counter += 1
-    return()
+    #remove the additional bias column again!
+    del X['bias']
+    return(weights, Rsquare(y, yPred))
 
+def predictLinearRegression(X, weights):
+    NVal = X.count()[0]
+    NVar = len(X.columns)
+    biasCol = np.ones(NVal)
+    X.insert(loc = NVar, column = 'bias', value = biasCol)
+    yPred = X.multiply(weights, axis = 1).sum(axis = 1)
+    #remove the additional bias column again!
+    del X['bias']
+    return(yPred)
+
+def matrixSolution(X, y):
+    #matrix equation that minimizes the sum of squares (Soq)
+    # ->dSoq/dw_i = 0
+    NVar = len(X.columns)
+    NVal = X.count()[0]
+    biasCol = np.ones(NVal)
+    X.insert(loc = NVar, column = 'bias', value = biasCol)
+    NVar += 1 
+    dS = np.zeros(NVar)
+    wCoeffs = np.zeros((NVar,NVar)) 
+    #w = ?
+    for i in range(0, NVar):
+        dS[i] = float(2*y.multiply(X.iloc[:,i], axis=0).sum(axis=0))
+    for i in range(0, NVar):
+        for j in range(0, NVar):
+            wCoeffs[i,j] = 2*float(X.iloc[:,i].multiply(X.iloc[:,j]).sum(axis = 0)) 
+    
+    weights = scipy.linalg.solve(wCoeffs, dS) #<- uses LU decomposition therefore faster than inverting matrix...
+    
+    #remove the additional bias column again!
+    del X['bias']
+    return(weights)
 
 if __name__ == '__main__':
     path = '../MetroInterstateTrafficVolume/MetroInterstateTrafficVolume.csv'
@@ -119,32 +195,59 @@ if __name__ == '__main__':
     #y = data['traffic_volume']
     #X = pd.DataFrame(data = np.array([[1, 1], [1, 2], [2, 2], [2, 3]]))
     #y = pd.DataFrame(data = (np.dot(X, np.array([1, 2])) + 3))
-    x = np.linspace(0,200, num = 200)
+    N1 = 20000
+    x = np.linspace(0,200, num = N1)
     x1 = x+2
-    x2 = x*4-3
-    x3 = x*15-7
-    xges = np.zeros((200,3))
-    for i in range(0, 200):
-        xges[i, 0] = x1[i]
-        xges[i, 1] = x2[i]
-        xges[i, 2] = x3[i]
+    #x2 = x*4-3
+    #x3 = x*15-7
+    N2 = 1
+    #N2 = 3
+    xges = np.zeros((N1,N2))
+    xges[:, 0] = x1[:]
+    #xges[i, 1] = x2[i]
+    #xges[i, 2] = x3[i]
     X = pd.DataFrame(data = xges)
-    y = pd.DataFrame(data = (np.dot(X, np.array([1, 2, 3])) + 4))
+    y = pd.DataFrame(data = (np.dot(X, np.array([2])) + 4))
     
     
     print(X)
     print(y)
-
+    
+    print('==================================================')
+    
+    start = time.time()
     reg = LinearRegression(normalize = False).fit(X, y)
     print('reg.score(X,y) =', reg.score(X, y))
     print('reg.coef_ = ', reg.coef_)
     print('reg.intercept_ = ',reg.intercept_)
+    end = time.time()
+    print('time = ', end - start)
     
     print('==================================================')
     
-    linearRegression(X, y, alpha = 1e-9, convergenceCriterion = 1e-5, adaptAlpha = True, printOutput = True)
+    start = time.time()
+    #alpha = [1e-9, 1e-9, 1e-9, 2.5e-6]
+    alpha = [5e-4, 5e-2]
+    #mu = 1e-2
+    mu = 1
+    weights, score = linearRegression(X, y, alpha = alpha, mu = mu, convergenceCriterion = 1e-4, 
+                                             lossFunction = 'MSE', alphaMethod = 'sqrt', printOutput = False)
+    yPred = predictLinearRegression(X, weights)
+    #print('yPred = ', yPred)
+    print('weights = ', weights)
+    print('score = ', score)
+    end = time.time()
+    print('time = ', end - start)
+
+    print('==================================================')
+    
+    start = time.time()
+    weights = matrixSolution(X, y)
+    print('weights = ', weights)
+    end = time.time()
+    print('time = ', end - start)
     
     
-
-
+    
+    
 
