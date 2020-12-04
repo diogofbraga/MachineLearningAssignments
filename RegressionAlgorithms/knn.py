ad9cba6829ac1d5c51f04f4d85fa16f48c6aebb4
diogofbraga <diogofiliperfbraga@gmail.com>
2020-12-04 20:34:10 +0100

    KNN: Radius & Correction on the set of neighbours

--- RegressionAlgorithms/knn.py
+++ RegressionAlgorithms/knn.py
@@ -7,41 +7,48 @@ def read_data(path):
     read_file = open(path, newline='')
     data = csv.DictReader(read_file)
     return data
+
+def distance_calculation(row):
+    if distance_function == 1: # Euclidean
+        if len(features) == 1:
+            distance = abs(float(row[features[0]])-float(target[features[0]]))
+        elif len (features) > 1:
+            features_sum = 0
+            for x in features:
+                features_sum = features_sum + pow(float(row[x])-float(target[x]),2)
+            distance = sqrt(features_sum)
+    elif distance_function == 2: # Manhattan
+        if len(features) == 1:
+            distance = abs(float(row[features[0]])-float(target[features[0]]))
+        elif len (features) > 1:
+            features_sum = 0
+            for x in features:
+                features_sum = features_sum + abs(float(row[x])-float(target[x]))
+            distance = features_sum
+    return distance
         
 def analyse(data, n_neighours):
-    shortest = 1000
     neighbours = []
-    
+
     for row in data:
-        #print(row)
-        if distance_function == 1:
-            if len(features) == 1:
-                distance = abs(float(row[features[0]])-float(target[features[0]]))
-            elif len (features) > 1:
-                features_sum = 0
-                for x in features:
-                    features_sum = features_sum + pow(float(row[x])-float(target[x]),2)
-                distance = sqrt(features_sum)
-        elif distance_function == 2:
-            if len(features) == 1:
-                distance = abs(float(row[features[0]])-float(target[features[0]]))
-            elif len (features) > 1:
-                features_sum = 0
-                for x in features:
-                    features_sum = features_sum + abs(float(row[x])-float(target[x]))
-                distance = features_sum
-        if distance <= shortest:
-            shortest = distance
-            best = {'distance': shortest, 'row': row}
-            neighbours.insert(0, best)
-            if len(neighbours) > n_neighours:
-                neighbours.pop()
-            #print(shortest)
+        #print(row) 
+        distance = distance_calculation(row)
+        if mode == 1:
+            if (len(neighbours) < n_neighours) or (distance <= neighbours[0]['distance']):
+                best = {'distance': distance, 'row': row}
+                neighbours.insert(0, best)
+                if len(neighbours) > n_neighours:
+                    neighbours.pop()
+        elif mode == 2:
+            if distance <= radius:
+                best = {'distance': distance, 'row': row}
+                neighbours.append(best)
     
     print("Neighbours:")
     for x in neighbours:
         print("Distance:", x['distance'])
         print("Row:", x['row'])
+
     return neighbours
 
 def calculate(neighbours):
@@ -56,10 +63,12 @@ def calculate(neighbours):
 
 # Global variables
 path = '../MetroInterstateTrafficVolume/MetroInterstateTrafficVolume.csv'
+mode = 1 # 1 = KNeighbors; 2 = RadiusNeighbors
 n_neighours = 5
 distance_function = 1 # 1 = Euclidean Distance; 2 = Manhattan Distance
+radius = 0 # 0 indicates no radius
 label = 'traffic_volume'
-features = ['temp', 'clouds_all', 'rain_1h', 'snow_1h']
+features = ['temp']
 target = {'holiday': 'None', 'temp': '276.42', 'rain_1h': '0.0', \
         'snow_1h': '0.0', 'clouds_all': '20', 'weather_main': 'Haze', \
         'weather_description': 'haze', 'date_time': '2016-02-19 00:00:00', 'traffic_volume': '708'}
